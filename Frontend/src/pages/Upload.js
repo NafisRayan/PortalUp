3d00188cec93153725a4e4a9ddc7a68f5dc7b830
Nafis Rayan <nafisrayan123@gmail.com>
2025-01-03 04:50:15 +0600

    `Refactor FileUpload component and Upload page with minor styling adjustments`

--- Frontend/src/pages/Upload.js
+++ Frontend/src/pages/Upload.js
@@ -3,9 +3,9 @@ import FileUpload from "../components/FileUpload";
 
 const Upload = () => {
   return (
-    <div className="h-screen bg-gradient-to-br from-pink-600 to-indigo-800 flex justify-center items-center p-4 md:p-8">
-    <div className="bg-black bg-opacity-70 rounded-lg shadow-xl p-6 md:p-12 m-4 w-full backdrop-blur-md animate-zoom-in">
-        <h1 className="text-white font-bold text-3xl md:text-4xl text-center mb-6 md:mb-8 animate-pulse">
+    <div className="min-h-screen bg-gradient-to-br from-pink-600 to-indigo-800 flex justify-center items-center p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
+      <div className="bg-black bg-opacity-70 rounded-lg shadow-xl p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 m-2 sm:m-4 w-full  backdrop-blur-md animate-zoom-in">
+        <h1 className="text-white font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center mb-4 sm:mb-6 md:mb-8 lg:mb-10 animate-pulse">
           <span className="text-pink-400">Upload</span> Your Files
         </h1>
         <div className="animate-fade-in">
